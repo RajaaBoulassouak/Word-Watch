04ea186bf657d734c9c44c6b9a60d3f4e3a653a2
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-05 09:56:33 -0700

    Create POST request to add a word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import $ from 'jquery'
+var wordInput = document.querySelector('.input-field')
 
-$(document).ready(() => {
-  // have fun!
-})
+
+function addWord () {
+  var word = wordInput.value
+  var addWordRequest = new XMLHttpRequest();
+  addWordRequest.open('POST', 'https://wordwatch-api.herokuapp.com/api/v1/words');
+  addWordRequest.setRequestHeader('Content-Type', 'application/json');
+  addWordRequest.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
+      alert(`${word} added`);
+    };
+  };
+  var wordObject = {"word": { "value": "sample" }};
+  var wordString = JSON.stringify(wordObject);
+  addWordRequest.send(jsonString)
+}

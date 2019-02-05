3f6f84651ea075f62a645473d0ce3dc29301b5fe
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-05 10:33:30 -0700

    Create GET request to get top_word

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 var wordInput = document.querySelector('.input-field')
 var breakDownButton = document.querySelector('.break-down-btn')
 
+function getWord () {
+  var getWordRequest = new XMLHttpRequest();
+  getWordRequest.open('GET', 'https://wordwatch-api.herokuapp.com/api/v1/top_word'); 
+  getWordRequest.setRequestHeader('Content-Type', 'application/json');
+  getWordRequest.onload = function() {
+    var response = JSON.parse(getWordRequest.responseText);
+    console.log(response['word']);
+  };
+  getWordRequest.send();
+};
+
 function addWord () {
   var word = wordInput.value
-  debugger
   var addWordRequest = new XMLHttpRequest();
   addWordRequest.open('POST', 'https://wordwatch-api.herokuapp.com/api/v1/words');
   addWordRequest.setRequestHeader('Content-Type', 'application/json');
@@ -16,4 +26,5 @@ function addWord () {
   addWordRequest.send(wordObject)
 }
 
+window.addEventListener('load', getWord);
 breakDownButton.addEventListener('click', addWord);
